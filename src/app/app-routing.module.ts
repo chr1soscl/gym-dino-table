0f396cac5da33ab6a7f04c40d317c142d599945b
chr1soscl <chr1so.scl@gmail.com>
2022-07-14 19:28:24 -0400

    other folders

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { PokemonComponent } from './components/pokemon/pokemon.component';
 
 const routes: Routes = [
   {
     path:"",
     component:AppComponent
   },
-  {
-    path:"home",
-    component:AppComponent
-  }
+  { path:"home", component:AppComponent },
+  { path:"pokemon", component:PokemonComponent},
+  { path:"movies", component:MoviesComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { DinoTableModule } from 'dino-table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonComponent } from './components/pokemon/pokemon.component';
+import { MoviesComponent } from './components/movies/movies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
